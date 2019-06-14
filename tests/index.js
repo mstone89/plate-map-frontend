682ded13bc955de790779e10984a44729d0b4e4b
Molly Stone <molly.stone89@gmail.com>
2019-06-14 12:14:07 -0400

    Format test data into grid array

--- tests/index.js
+++ tests/index.js
@@ -1,77 +1,8 @@
-var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
-//
-// // d3.select('h1')
-// //     .style('color', 'red')
-// //     .attr('class', 'heading')
-// //     .text('Update h1 tag');
-//
-// // d3.select('body').append('p').text('1st Paragraph');
-// // d3.select('body').append('p').text('2nd Paragraph');
-// // d3.select('body').append('p').text('3rd Paragraph');
-//
-// // d3.selectAll('p').style('color', 'blue');
-//
-// // d3.select('body')
-// //     .selectAll('p')
-// //     .data(dataset)
-// //     .enter()
-// //     .append('p')
-// //     // .text('Appending based on dataset');
-// //     .text((d) => { return d }); // remember to return
-//
-// let svgWidth = 500, svgHeight = 300, barPadding = 5;
-// let barWidth = (svgWidth / dataset.length);
-//
-// let svg = d3.select('svg')
-//     .attr('width', svgWidth)
-//     .attr('height', svgHeight)
-//     .style('background-color', 'lightgray');
-//
-// let barChart = svg.selectAll('rect')
-//     .data(dataset)
-//     .enter()
-//     .append('rect')
-//     .attr('y', (d) => { return svgHeight - d })
-//     .attr('height', (d) => { return d })
-//     .attr('width', barWidth - barPadding)
-//     .attr('transform', (d, i) => {
-//         let translate = [barWidth * i, 0];
-//         return `translate(${translate})`;
-//     })
-//     .attr('class', 'bar');
-//
-// var text = svg.selectAll("text")
-//     .data(dataset)
-//     .enter()
-//     .append("text")
-//     .text(function(d) {
-//         return d;
-//     })
-//     .attr("y", function(d, i) {
-//         return svgHeight - d - 2;
-//     })
-//     .attr("x", function(d, i) {
-//         return barWidth * i;
-//     })
-//     .attr("fill", "#A64C38");
-//
-// let svg = d3.select('svg')
-//     .attr('width', 500)
-//     .attr('height', 500)
-//     .style('background-color', 'cornflowerblue')
-//
-// svg.selectAll('circle')
-//     .data([40, 80, 120])
-//     .enter()
-//     .append('circle')
-//     .attr('cx', (d) => { return d })
-//     .attr('cy', 60)
-//     .attr('r', 10)
-//
-// let circle = d3.selectAll('circle')
-//     .style('fill', 'blue')
+// /////////////////////////
+// TEST GRID
+// /////////////////////////
 
-// const gridData = () => {
+// const testGrid = () => {
 //     const data = [];
 //     let x = 1;
 //     let y = 1;
@@ -95,7 +26,8 @@ var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 //     return data;
 // }
 //
-// let data = gridData();
+// let data = testGrid();
+// console.log(data);
 //
 // let grid = d3.select('#grid')
 //     .append('svg')
@@ -121,6 +53,10 @@ var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
 //     .style('fill', '#fff')
 //     .style('stroke', '#222');
 
+///////////////////////////
+// PLATE TEST
+///////////////////////////
+
 let colors = [
     'tomato', 'orange', 'violet', 'cornflowerblue',
     'slateblue', 'mediumseagreen', 'aqua', 'chartreuse',
@@ -129,13 +65,13 @@ let colors = [
 ]
 
 let standardsBlank = [
-    {name: "stanard 1", color: 'yellow'},
-    {name: "stanard 2", color: 'yellow'},
-    {name: "stanard 3", color: 'yellow'},
-    {name: "stanard 4", color: 'yellow'},
-    {name: "stanard 5", color: 'yellow'},
-    {name: "stanard 6", color: 'yellow'},
-    {name: "stanard 7", color: 'yellow'},
+    {name: "standard 1", color: 'yellow'},
+    {name: "standard 2", color: 'yellow'},
+    {name: "standard 3", color: 'yellow'},
+    {name: "standard 4", color: 'yellow'},
+    {name: "standard 5", color: 'yellow'},
+    {name: "standard 6", color: 'yellow'},
+    {name: "standard 7", color: 'yellow'},
     {name: 'blank', color: 'white'}
 ];
 
@@ -155,7 +91,6 @@ const createSampleData = (sampleNum, replicates) => {
 
 const addDilution = (dilutionNum, sampleData) => {
     let dilutionData = [];
-    console.log(sampleData);
     for (let i = 0; i < dilutionNum; i++) {
         for (let j = 0; j < sampleData.length; j++) {
             dilutionData.push(sampleData[j]);
@@ -180,12 +115,11 @@ const createFinalData = (dilutionNum, sampleNum, replicates) => {
     );
 }
 
-console.log(createFinalData(2, 12, 3));
-
 const gridData = () => {
-    let data = [];
-    let x = 5;
-    let y = 5;
+    let finalData = createFinalData(2, 12, 3);
+    let dataForGrid = [];
+    let x = 1;
+    let y = 1;
     let width = 50;
     let height = 50;
 
@@ -193,20 +127,26 @@ const gridData = () => {
     let columns = 12;
 
     for (let row = 0; row < rows; row++) {
-        data.push([]);
+        dataForGrid.push([]);
         for (let column = 0; column < columns; column++) {
-            data[row].push({
-                x: x,
-                y: y,
-                width: width,
-                height: height
-            });
+            dataForGrid[row].push(finalData.shift());
+        }
+    }
+
+    for (let row = 0; row < rows; row++) {
+        for (let column = 0; column < columns; column++) {
+            dataForGrid[row][column].x = x;
+            dataForGrid[row][column].y = y;
+            dataForGrid[row][column].width = width;
+            dataForGrid[row][column].height = height;
             x += width;
         }
-        x = 5;
+        x = 1;
         y += height;
     }
-    return data;
+
+    console.log(dataForGrid);
+    return dataForGrid;
 }
 
 let grid = d3.select('#grid')
