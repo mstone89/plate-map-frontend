2b3c6fa8108c9bf908f42c7e0101125d416b8025
Molly Stone <molly.stone89@gmail.com>
2019-06-15 13:21:58 -0400

    Finish creating saved plates component

--- src/components/SavedPlates.js
+++ src/components/SavedPlates.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Media } from 'react-bootstrap';
 
 class SavedPlates extends Component {
     constructor(props) {
@@ -12,6 +14,7 @@ class SavedPlates extends Component {
         fetch('http://localhost:3000/plates')
             .then(data => data.json())
             .then(jsonData => {
+                console.log(jsonData);
                 this.setState({
                     savedPlates: jsonData
                 });
@@ -26,6 +29,21 @@ class SavedPlates extends Component {
         return (
             <div className="saved-plates">
                 <h3>Saved Plates</h3>
+                {this.state.savedPlates.map((plate, index) => {
+                    const link = `/view/${plate.id}`;
+                    return (
+                        <Media key={index}>
+                            <Media.Body>
+                                <Link to={link}><h5>{plate.name}</h5></Link>
+                                <p>
+                                    Samples: {plate.samples} /
+                                    Replicates: {plate.replicates} /
+                                    Dilutions: {plate.dilutions}
+                                </p>
+                            </Media.Body>
+                        </Media>
+                    );
+                })}
             </div>
         );
     }
