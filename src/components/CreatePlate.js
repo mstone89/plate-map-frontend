0355deb996d30260f634a091252b9e40f194dc8c
Molly Stone <molly.stone89@gmail.com>
2019-06-22 10:59:23 -0400

    Update headers

--- src/components/CreatePlate.js
+++ src/components/CreatePlate.js
@@ -44,14 +44,14 @@ class CreatePlate extends Component {
         return (
             <div className="main-container">
                 <aside>
-                    <h2>Generating Plates</h2>
+                    <h2>How It Works</h2>
                     <p>Sometimes the most difficult part of an experiment is that first step. Plate-Map is here to assit in planning out your assays without a lot of guesswork.  Simply input the number of samples you are testing, and Plate-Map does the rest.</p>
                     <p>Plate-Map uses a typical 96 well plate format, with a 7 point standard curve and up to 16 samples. With sample input, the app will suggest dilutions, replicates, and even well count, so you can maximize each and every experiment. It will offer two possible map options upon sample input.</p>
                     <p>Plate-Map is an experimental planning tool that will save money and increase efficiency for costly and time consuming assay work, a must have for any modern lab.</p>
                 </aside>
                 <div className="right-column">
                     <Form onSubmit={this.handleSubmit}>
-                        <h2 className="create-header">Create New Plates</h2>
+                        <h2 className="create-header">Generating Plates</h2>
                         <Form.Group className="create-form">
                             <Form.Label>Number Of Samples:</Form.Label>
                             <Form.Control
